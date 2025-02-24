176316a011d91a6b48c5ed01741f21cb1dff622b
sbc2c <sreeharshab2010@gmail.com>
2025-02-24 17:48:23 -0600

    1st commit
    👍

--- Problem3.py
+++ Problem3.py
@@ -1,1 +1,13 @@
+#defining function
+def even(num):
+  #seeing if num / 2 = 0
+  #only even nums can equal 0 when using a modulo
+    if num % 2 == 0:
+      #print statement
+        print('True')
+    else:
+      #print statment
+        print('False')
+#calling of function
+even(4)
 
