3772553a092711d699e7e9db8292d7f86693fd2d
EvaE-06 <evaerasme64@gmail.com>
2025-02-24 19:01:31 -0500

    Update Problem4.py- EVA

--- Problem4.py
+++ Problem4.py
@@ -1,1 +1,16 @@
+def fizzBuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz") #divisible by 3 and 5
+        elif i % 3 == 0:
+            result.append("Fizz") #divisible by 3
+        elif i % 5 == 0:
+            result.append("Buzz")# divisible by 5
+        else:
+            result.append(str(i))
+    return result
 
+n = 5 #how many terms in the list 
+output = fizzBuzz(n)
+print(output)
